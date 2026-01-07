63d515104fac7c94b6aa14130a723b6b96d0e238
hoseinx@gmail.com <hoseinx@gmail.com>
2026-01-07 08:03:34 +0330

    config

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import 'leaflet/dist/leaflet.css';
+import './lib/leaflet-icon';
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

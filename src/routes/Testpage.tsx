789c949f9cfaddc4bd0413edb6cdb0aab40f9e72
hoseinx@gmail.com <hoseinx@gmail.com>
2026-01-27 09:21:00 +0330

    use class

--- src/routes/Testpage.tsx
+++ src/routes/Testpage.tsx
@@ -12,6 +12,41 @@ interface User {
   email: string;
 }
 
+class UserClass {
+  private id: number;
+  name: string;
+  age: number;
+  email: string;
+
+  constructor(name: string, age: number, email: string) {
+    this.id = Date.now();
+    this.age = age;
+    this.name = name;
+    this.email = email;
+  }
+
+  getUserInfo() {
+    console.log(
+      `id  ${this.id}  -  name  ${this.name} email ${this.email}  age ${this.age}  `
+    );
+  }
+
+  updateValue(id: number) {
+    this.id = id;
+    this.getUserInfo();
+  }
+}
+
+class Amin extends UserClass {
+  constructor(name: string, age: number, email: string) {
+    super(name, age, email);
+  }
+
+  public getAdminData() {
+    console.log(`${this.id} ${this.name} ${this.email}`);
+  }
+}
+
 const defUserValue: User = {
   id: 0,
   name: "",
@@ -35,6 +70,14 @@ const TestPage = () => {
     setUsersData((prevState) => [...prevState, { ...user, id: Date.now() }]);
     setUserData(defUserValue);
   }
+
+  const user1 = new UserClass("reza", 45, "aa@aa.aa");
+  user1.getUserInfo();
+  user1.updateValue(22);
+
+  const admin = new Amin("reza", 45, "aa@aa.aa");
+  admin.getAdminData();
+  // console.log(user1.id);
 
   return (
     <>
